93b96a6d21142ac17a86377f15caaa116a753b22
t-cochran <cochran@colorado.edu>
2020-06-11 23:23:30 -0600

    Added "Create Trip" and "View Trip" buttons to homepage

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -6,5 +6,5 @@ from . import views
 # Link 'pages' app urls to views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('trips/', views.TripPageView.as_view(), name='triplist'),
+    path('trips/', views.TripPageView.as_view(), name='trip_list'),
 ]
