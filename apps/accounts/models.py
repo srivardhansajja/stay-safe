cbff35135368a2ff6c81f24a65e850a62542be61
t-cochran <cochran@colorado.edu>
2020-06-15 02:08:06 -0600

    Bug Fix: Signup emails are unique to each registered user

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -8,5 +8,11 @@ class CustomAccount(AbstractUser):
     """Set custom user account fields"""
     emergency_email = models.EmailField(blank=True, max_length=254)
 
+    email = models.EmailField(
+            verbose_name='email address',
+            max_length=255,
+            unique=True,
+    )
+
     def __str__(self):
         return self.username
