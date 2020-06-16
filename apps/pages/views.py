c5ebebf739a4a6ea43a8b92f6bac96b80dd0aa4f
t-cochran <cochran@colorado.edu>
2020-06-15 18:32:38 -0600

    Added LoginRequiredMixin to redirect unauthenticated users to the login page

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,4 +1,5 @@
 # apps/pages/views.py
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
@@ -12,20 +13,23 @@ class HomePageView(TemplateView):
 
 
 # Render the page to View Trips
-class TripPageView(ListView):
+class TripPageView(LoginRequiredMixin, ListView):
     model = Trip
     template_name = 'trip_view.html'
     fields = '__all__'
+    login_url = '/accounts/login/'
 
     def get_queryset(self):
         return Trip.objects.filter(trip_owner=self.request.user)
 
+
 # Render the page to Create Trips
-class TripCreateView(CreateView):
+class TripCreateView(LoginRequiredMixin, CreateView):
     model = Trip
     template_name = 'trip_create.html'
     form_class = TripCreateForm
     success_url = reverse_lazy('home')
+    login_url = '/accounts/login/'
 
     def form_valid(self, form):
         form.instance.trip_owner = self.request.user
