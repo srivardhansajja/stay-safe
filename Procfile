eec587f89368db24da63ee4d91f23ae473140baf
Thomas Dolan <tdolanindiana@yahoo.com>
2020-06-19 13:15:44 -0600

    tjd_prepped_repo_for_deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn _stay_safe.wsgi
--- _stay_safe/settings.py
+++ _stay_safe/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from decouple import config, Csv
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -148,3 +149,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
