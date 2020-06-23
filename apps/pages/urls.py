3f8498fe77cb38c99c0c911054414b1962a8f1ee
t-cochran <cochran@colorado.edu>
2020-06-23 15:40:40 -0600

    Re-formatted and organized pages/urls

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -6,19 +6,45 @@ from . import views
 
 # Link 'pages' app urls to views
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
-    path('trips/', views.TripPageView.as_view(), name='trip_list'),
-    path('trips/add/', views.TripCreateView.as_view(), name='trip_create'),
-    path('password_reset/',
-         auth_views.PasswordResetView.as_view(),
-         name='password_reset'),
-    path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'),
-    path('password_reset_confirm/',
-         auth_views.PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
-    path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'),
+
+    # Homepage
+    path(
+        '',
+        views.HomePageView.as_view(),
+        name='home'
+    ),
+
+    # Add/View Trips
+    path(
+        'trips/',
+        views.TripPageView.as_view(),
+        name='trip_list'
+    ),
+    path(
+        'trips/add/',
+        views.TripCreateView.as_view(),
+        name='trip_create'
+    ),
+
+    # Password Reset
+    path(
+        'password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    path(
+        'password_reset_confirm/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
 ]
