6ff5d79de11f44c1d927e4196d4ea3e5bc1d147c
t-cochran <cochran@colorado.edu>
2020-06-25 19:44:21 -0600

    Removed emergency_email field from CustomAccount model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,14 +1,11 @@
 # apps/accounts/models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from enum import Enum
 
 
 # Create custom accounts for the database
 class CustomAccount(AbstractUser):
     """Set custom user account fields"""
-    emergency_email = models.EmailField(blank=True, max_length=254)
-
     email = models.EmailField(
             verbose_name='email address',
             max_length=255,
