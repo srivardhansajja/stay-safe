1c92175bfe991435da0aa15cb8a925ada69122f8
t-cochran <cochran@colorado.edu>
2020-06-25 21:29:22 -0600

    updated apps.accounts.tests.test_forms

--- apps/accounts/tests/test_forms.py
+++ apps/accounts/tests/test_forms.py
@@ -5,6 +5,13 @@ from ..forms import CustomCreateForm
 class CustomCreateFormTest(TestCase):
     def test_form_fields(self):
         form = CustomCreateForm()
-        expected = ['username', 'email', 'password1', 'password2']
+        expected = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2'
+        ]
         actual = list(form.fields)
         self.assertSequenceEqual(expected, actual)
