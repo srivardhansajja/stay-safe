267756b3f6ae9ce892e6d4965fde2abd6b22f910
Thomas Dolan <tdolanindiana@yahoo.com>
2020-07-13 17:32:54 -0400

    Ported script helper methods to models

--- apps/pages/management/commands/script.py
+++ apps/pages/management/commands/script.py
@@ -7,85 +7,6 @@ from apps.pages.models import TripStatusList_, Trip, EmergencyContact
 
 
 class Command(BaseCommand):
-    def update_status(self, trip):
-        """
-        Update the status of the trip object in the database.
-        """
-        # Get trip date information
-        start = trip.trip_start
-        end = trip.trip_end
-        now = timezone.now()
-
-        # Upcoming
-        if (start > now and start <= now + timedelta(days=7)):
-            trip.trip_status = TripStatusList_.YTS.value
-
-        # In progress
-        if (start <= now and end > now):
-            trip.trip_status = TripStatusList_.IP.value
-
-        # Awaiting Response
-        if (end < now and end > now - timedelta(hours=1)):
-            trip.trip_status = TripStatusList_.AR.value
-
-        # Complete
-        if (end < now - timedelta(hours=1)):
-            trip.trip_status = TripStatusList_.CP.value
-
-        # Save changes in the database
-        trip.save()
-
-    def send_notificationEmail(self, owner, trip):
-        """
-        Send an email to the trip owner when a trip is 'Awaiting response'.
-        """
-        # Define email fields
-        n = '\n\n'
-        sender = 'staysafe3308@gmail.com'
-        subject = f'Awaiting response for trip to: {trip.trip_location}!'
-        emergency_contact_date = (trip.trip_end + timedelta(hours=1)).strftime(
-            '%l:%m%p on %b %d, %Y'
-        )
-
-        # Send an email notifying the user that a trip is awaiting response
-        message = \
-            f'Hello {owner.first_name} {owner.last_name},{n}'\
-            f'Your trip to {trip.trip_location} is awaiting your response.{n}'\
-            f'If you do not mark this trip as complete, then your emergency '\
-            f'contacts will be notified at: {emergency_contact_date} {n}'\
-            f'{n}'\
-            f'-The Stay Safe Team'
-        send_mail(subject, message, sender, [owner.email])
-
-    def send_contactEmails(self, owner, trip):
-        """
-        Send an email to all of the trip owner's emergency contacts.
-        """
-        # Define email fields
-        n = '\n\n'
-        name_list = [
-            c.first_name for c in EmergencyContact.objects.filter(user=owner)
-        ]
-        email_list = [
-            c.email for c in EmergencyContact.objects.filter(user=owner)
-        ]
-        end_date = trip.trip_end.strftime('%l:%m%p on %b %d, %Y')
-        sender = 'staysafe3308@gmail.com'
-        subject = f'{owner.first_name} {owner.last_name}\'s trip ended!'
-
-        # Send an email to each emergency contact
-        for contact_name, contact_email in zip(name_list, email_list):
-            message = \
-                f'Hello {contact_name},{n}'\
-                f'{owner.first_name} {owner.last_name}\'s trip to '\
-                f'{trip.trip_location} ended 1 hour ago at {end_date}.{n}'\
-                f'If you have not heard from them yet, consider contacting'\
-                f' them to make sure they are safe.{n}'\
-                f'Their email address is: {owner.email}'\
-                f'{n}'\
-                f'-The Stay Safe Team'
-            send_mail(subject, message, sender, [contact_email])
-
     def handle(self, *args, **kwargs):
         """
         The main logic of this script.
@@ -93,7 +14,7 @@ class Command(BaseCommand):
             When run, this script is responsible for executing three methods:
                 (1) Update the trip status of all trips.
                 (2) Email the user if a trip is "Awaiting response".
-                (3) Email emergency contacts if there is no user resopnse.
+                (3) Email emergency contacts if there is no user response.
 
             The return statement at the end of the for-loop is required. It
             lets the Heroku Scheduler know when it can shut down.
@@ -101,21 +22,21 @@ class Command(BaseCommand):
         for trip in Trip.objects.all():
             # Ignore trips that have completed and sent a response to contacts
             current_status = trip.trip_status
-            if (current_status == "Completed" and trip.response_sent is True):
+            if current_status == "Completed" and trip.response_sent:
                 continue
 
             # Update the trip's status
-            self.update_status(trip)
+            trip.update_status()
 
             # Send an email to the user if a trip is Awaiting response
             new = trip.trip_status
-            if (new == "Awaiting response" and trip.notification is False):
-                self.send_notificationEmail(trip.trip_owner, trip)
+            if new == "Awaiting response" and not trip.notification:
+                trip.trip_owner.send_notificationEmail(trip)
                 trip.notification = True
 
             # Send an email to contacts if a trip completes with no response
-            if (new == "Completed" and trip.response_sent is False):
-                self.send_contactEmails(trip.trip_owner, trip)
+            if new == "Completed" and not trip.response_sent:
+                trip.trip_owner.send_contactEmails(trip)
                 trip.response_sent = True
             trip.save()
         return
--- apps/pages/models.py
+++ apps/pages/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.urls import reverse
 from apps.accounts.models import CustomAccount
 from enum import Enum
+from datetime import timedelta
+from django.utils import timezone
+from django.core.mail import send_mail
 
 
 # Class representing Trip Status, subclass of Enum
@@ -28,6 +31,85 @@ class Trip(models.Model):
     )
     response_sent = models.BooleanField(default=False)
     notification = models.BooleanField(default=False)
+
+    def update_status(self):
+        """
+        Update the status of the trip object in the database.
+        """
+        # Get trip date information
+        start = self.trip_start
+        end = self.trip_end
+        now = timezone.now()
+
+        # Upcoming
+        if now < start <= now + timedelta(days=7):
+            self.trip_status = TripStatusList_.YTS.value
+
+        # In progress
+        if start <= now < end:
+            self.trip_status = TripStatusList_.IP.value
+
+        # Awaiting Response
+        if end < now < end + timedelta(hours=1):
+            self.trip_status = TripStatusList_.AR.value
+
+        # Complete
+        if end < now - timedelta(hours=1):
+            self.trip_status = TripStatusList_.CP.value
+
+        # Save changes in the database
+        self.save()
+
+    def send_notification_email(self):
+        """
+        Send an email to the trip owner when a trip is 'Awaiting response'.
+        """
+        # Define email fields
+        n = '\n\n'
+        sender = 'staysafe3308@gmail.com'
+        subject = f'Awaiting response for trip to: {self.trip_location}!'
+        emergency_contact_date = (self.trip_end + timedelta(hours=1)).strftime(
+            '%l:%m%p on %b %d, %Y'
+        )
+
+        # Send an email notifying the user that a trip is awaiting response
+        message = \
+            f'Hello {self.trip_owner.first_name} {self.trip_owner.last_name},{n}' \
+                f'Your trip to {self.trip_location} is awaiting your response.{n}' \
+                f'If you do not mark this trip as complete, then your emergency ' \
+                f'contacts will be notified at: {emergency_contact_date} {n}' \
+                f'{n}' \
+                f'-The Stay Safe Team'
+        send_mail(subject, message, sender, [self.trip_owner.email])
+
+    def send_contact_emails(self):
+        """
+        Send an email to all of the trip owner's emergency contacts.
+        """
+        # Define email fields
+        n = '\n\n'
+        name_list = [
+            c.first_name for c in EmergencyContact.objects.filter(user=self.trip_owner)
+        ]
+        email_list = [
+            c.email for c in EmergencyContact.objects.filter(user=self.trip_owner)
+        ]
+        end_date = self.trip_end.strftime('%l:%m%p on %b %d, %Y')
+        sender = 'staysafe3308@gmail.com'
+        subject = f'{self.trip_owner.first_name} {self.trip_owner.last_name}\'s trip ended!'
+
+        # Send an email to each emergency contact
+        for contact_name, contact_email in zip(name_list, email_list):
+            message = \
+                f'Hello {contact_name},{n}' \
+                    f'{self.trip_owner.first_name} {self.trip_owner.last_name}\'s trip to ' \
+                    f'{self.trip_location} ended 1 hour ago at {end_date}.{n}' \
+                    f'If you have not heard from them yet, consider contacting' \
+                    f' them to make sure they are safe.{n}' \
+                    f'Their email address is: {self.trip_owner.email}' \
+                    f'{n}' \
+                    f'-The Stay Safe Team'
+            send_mail(subject, message, sender, [contact_email])
 
     def __str__(self):
         return self.trip_name
